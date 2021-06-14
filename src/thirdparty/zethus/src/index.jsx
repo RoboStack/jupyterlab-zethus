63f1d788c2aa62fa1124520ef095bd73b363d953
Wolf Vollprecht <w.vollprecht@gmail.com>
2021-06-14 09:36:51 +0200

    jlab3 compatible release (#2)

--- src/thirdparty/zethus/src/index.jsx
+++
@@ -1,8 +0,0 @@
-import ReactDOM from 'react-dom';
-import React from 'react';
-
-import Zethus from './zethus';
-
-import './styles/common.scss';
-
-ReactDOM.render(<Zethus />, document.getElementById('root'));
